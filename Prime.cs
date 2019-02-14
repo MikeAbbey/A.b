59dee8928fb1c7e191e437293440e1cad86eb876
MikeAbbey <47626243+MikeAbbey@users.noreply.github.com>
2019-02-14 10:03:25 +0100

    Add files via upload

---
+++ Prime.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static int prime(int n)
+        {
+            int i,aux=0;
+            for (i = 2; i <= n / 2; i++)
+                if (n % i == 0)
+                    aux = 1;
+            return aux;
+                
+        }
+        static void Main(string[] args)
+        {
+            int n,m,i;
+            Console.WriteLine("Enter the inferior range: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the superior range: ");
+            m = int.Parse(Console.ReadLine());
+            for (i = n; i <= m; i++)
+                if (prime(i) == 0)
+                    Console.WriteLine(i);
+            Console.ReadLine();
+
+        }
+    }
+}
